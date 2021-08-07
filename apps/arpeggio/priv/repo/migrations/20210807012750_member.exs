21528bf1a2fb646fd8535eac5489d85ba9846283
Jan Blackquill <uhhadd@gmail.com>
2021-08-07 00:15:53 -0400

    license

--- apps/arpeggio/priv/repo/migrations/20210807012750_member.exs
+++ apps/arpeggio/priv/repo/migrations/20210807012750_member.exs
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2021 Carson Black <uhhadd@gmail.com>
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 defmodule Arpeggio.Repo.Migrations.Member do
   use Ecto.Migration
 
